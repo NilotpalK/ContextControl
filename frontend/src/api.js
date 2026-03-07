ab5baaabe0519bc62afa3aba47f4063821f29f45
Nilotpal <nilotpal@Nilotpals-MacBook-Air.local>
2026-03-08 03:33:36 +0530

    fix(frontend): Route API to VITE_API_URL instead of localhost

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000';
+const API_BASE = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace(/\/$/, '') : 'http://localhost:10000';
 
 export let GLOBAL_API_KEY = "";
 
